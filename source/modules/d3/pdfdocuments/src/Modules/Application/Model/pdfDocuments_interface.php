c9e9f8b8a6766da87e957a86d69f73d797c3dec1
MaxBUhe <mb@shopmodule.com>
2020-05-20 09:33:56 +0200

    rm: unneseccary interface file

--- source/modules/d3/pdfdocuments/src/Modules/Application/Model/pdfDocuments_interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('deliverynotePdf.php');
-require('invoicePdf.php');
-
-interface pdfDocuments_interface{
-  public function genPdf($sFilename, $iSelLang = 0, $target = 'I');
-  public function setOrder(\OxidEsales\Eshop\Application\Model\Order $order);
-  public function getOrder();
-}
