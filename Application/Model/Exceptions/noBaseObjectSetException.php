895ea19f89eccd8cc6237781e84ccc6ed6983e38
Daniel Seifert <ds@shopmodule.com>
2020-06-02 10:12:35 +0200

    improve code

--- Application/Model/Exceptions/noBaseObjectSetException.php
+++ Application/Model/Exceptions/noBaseObjectSetException.php
@@ -15,9 +15,11 @@
 
 namespace D3\PdfDocuments\Application\Model\Exceptions;
 
+use Exception;
+
 class noBaseObjectSetException extends pdfGeneratorExceptionAbstract
 {
-    public function __construct( $sMessage = "no base object (e.g. order) for pdf generator set", $iCode = 0, \Exception $previous = null )
+    public function __construct( $sMessage = "no base object (e.g. order) for pdf generator set", $iCode = 0, Exception $previous = null )
     {
         parent::__construct( $sMessage, $iCode, $previous );
     }
