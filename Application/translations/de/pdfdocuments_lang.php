9c12c8688b4373b54dc58eabfb22414a406b0bc5
Daniel Seifert <ds@shopmodule.com>
2020-06-30 10:33:23 +0200

    adjust multilang items

--- Application/translations/de/pdfdocuments_lang.php
+++ Application/translations/de/pdfdocuments_lang.php
@@ -12,6 +12,7 @@
  * @author        D3 Data Development <support@shopmodule.com>
  * @link          http://www.oxidmodule.com
  */
+ 
 $sLangName = "Deutsch";
 $aLang     = array(
     'charset'                                            => 'utf-8',
@@ -20,28 +21,28 @@ $aLang     = array(
     'D3_PDFDOCUMENTS_THANKYOU_2'                         => 'Ihr Team von %1$s',
 
     'D3_PDFDOCUMENTS_GET_IN_CONTACT'                     => 'So erreichen Sie uns:',
-    'D3_PDFDOCUMENTS_TELEFON'                            => 'Telefon: ',
-    'D3_PDFDOCUMENTS_FAX'                                => 'Fax: ',
-    'D3_PDFDOCUMENTS_EMAIL'                              => 'e-Mail: ',
+    'D3_PDFDOCUMENTS_TELEFON'                            => 'Telefon',
+    'D3_PDFDOCUMENTS_FAX'                                => 'Fax',
+    'D3_PDFDOCUMENTS_EMAIL'                              => 'e-Mail',
 
-    'D3_PDFDOCUMENTS_BANK_ACCOUNT'                       => 'Bankverbindung:',
-    'D3_PDFDOCUMENTS_BANK_ACCOUNTNR'                     => 'IBAN: ',
-    'D3_PDFDOCUMENTS_BANK_BANKCODE_HEADER'               => 'BIC: ',
+    'D3_PDFDOCUMENTS_BANK_ACCOUNT'                       => 'Bankverbindung',
+    'D3_PDFDOCUMENTS_BANK_ACCOUNTNR'                     => 'IBAN',
+    'D3_PDFDOCUMENTS_BANK_BANKCODE_HEADER'               => 'BIC',
 
-    'D3_PDFDOCUMENTS_ORDERBILLNR'                        => 'Rechnungs-Nr.: ',
-    'D3_PDFDOCUMENTS_CUSTOMERNR'                         => 'Kundennummer: ',
-    'D3_PDFDOCUMENTS_DATE'                               => 'Datum: ',
+    'D3_PDFDOCUMENTS_ORDERBILLNR'                        => 'Rechnungs-Nr.',
+    'D3_PDFDOCUMENTS_CUSTOMERNR'                         => 'Kundennummer',
+    'D3_PDFDOCUMENTS_DATE'                               => 'Datum',
     'D3_PDFDOCUMENTS_DATE_FORMAT'                        => '%d.%m.%Y',
-    'D3_PDFDOCUMENTS_USTIDNR'                            => 'Ust.-ID-Nr: ',
+    'D3_PDFDOCUMENTS_USTIDNR'                            => 'Ust.-ID-Nr.',
 
     'D3_PDFDOCUMENTS_INVOICE'                            => 'Rechnung',
     'D3_PDFDOCUMENTS_DELIVERYNOTE'                       => 'Lieferschein',
-    'D3_PDFDOCUMENTS_ORDERNR'                            => 'Bestellnr.: ',
+    'D3_PDFDOCUMENTS_ORDERNR'                            => 'Bestellnr.',
     'D3_PDFDOCUMENTS_ORDER_FROM_AT'                      => 'Ihre Bestellung vom %1$s bei "%2$s"',
 
     'D3_PDFDOCUMENTS_AMOUNT'                             => 'Menge',
     'D3_PDFDOCUMENTS_DESCRIPTION'                        => 'Beschreibung',
-    'D3_PDFDOCUMENTS_USTPERCENTAGE'                      => 'Ust. %: ',
+    'D3_PDFDOCUMENTS_USTPERCENTAGE'                      => 'Ust. %',
     'D3_PDFDOCUMENTS_UNITPRICE'                          => 'Einzelpreis',
     'D3_PDFDOCUMENTS_TOTALPRICE'                         => 'Gesamtpreis',
 
@@ -50,17 +51,17 @@ $aLang     = array(
     'D3_PDFDOCUMENTS_SUMBRUTTO'                          => 'Summe Artikel (brutto)',
     'D3_PDFDOCUMENTS_DISCOUNT'                           => 'Rabatt',
     'D3_PDFDOCUMENTS_SUMNETTO'                           => 'Summe Artikel (netto)',
-    'D3_PDFDOCUMENTS_TAX'                                => 'zzgl. MwSt. %1$s %% Betrag',
+    'D3_PDFDOCUMENTS_TAX'                                => 'zzgl. MwSt. %1$s %%',
     'D3_PDFDOCUMENTS_DELIVERY'                           => 'Versandkosten (netto)',
     'D3_PDFDOCUMENTS_TOTALSUMBRUT'                       => 'Gesamtsumme (brutto)',
 
-    'D3_PDFDOCUMENTS_USED_PAYMENTMETHOD'                 => 'Ihre gewählte Zahlungsweise: ',
+    'D3_PDFDOCUMENTS_USED_PAYMENTMETHOD'                 => 'Ihre gewählte Zahlungsweise',
     'D3_PDFDOCUMENTS_RECEIVED_PAYMENT'                   => 'Den Rechnungsbetrag haben wir dankend erhalten.',
 
-    'D3_PDFDOCUMENTS_MANAGINGDIRECTOR'                   => 'Geschäftsführer:',
+    'D3_PDFDOCUMENTS_MANAGINGDIRECTOR'                   => 'Geschäftsführer',
     'D3_PDFDOCUMENTS_COURT'                              => 'Amtsgericht ',
     'D3_PDFDOCUMENTS_HRBNR'                              => 'HRB ',
-    'D3_PDFDOCUMENTS_USTID'                              => 'Ust.-ID: ',
+    'D3_PDFDOCUMENTS_USTID'                              => 'Ust.-ID',
 
-    'D3_PDFDOCUMENTS_BANK_BANKCODE'                      => 'BIC-/SWIFT-Code: ',
+    'D3_PDFDOCUMENTS_BANK_BANKCODE'                      => 'BIC/SWIFT-Code',
 );
