52f904318e735515fa98b9b8404b9d1dd087a43d
Daniel Seifert <git@daniel-seifert.com>
2020-07-04 23:25:24 +0200

    apply GPL v3 license

--- Application/translations/de/pdfdocuments_lang.php
+++ Application/translations/de/pdfdocuments_lang.php
@@ -1,15 +1,10 @@
 <?php
 
 /**
- * This Software is the property of Data Development and is protected
- * by copyright law - it is NOT Freeware.
- * Any unauthorized use of this software without a valid license
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- * http://www.shopmodule.com
- *
+ * See LICENSE file for license details.
+ * 
  * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
- * @author        D3 Data Development <support@shopmodule.com>
+ * @author        D3 Data Development - Daniel Seifert <support@shopmodule.com>
  * @link          http://www.oxidmodule.com
  */
  
