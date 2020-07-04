d5b3b1a1a531dd7587f64be8775d6a1a2bf46a39
Daniel Seifert <git@daniel-seifert.com>
2020-07-04 23:25:24 +0200

    apply GPL v3 license

--- Application/Model/Registries/registryOrderoverviewInterface.php
+++ Application/Model/Registries/registryOrderoverviewInterface.php
@@ -1,18 +1,11 @@
 <?php
 
 /**
- * This Software is the property of Data Development and is protected
- * by copyright law - it is NOT Freeware.
- *
- * Any unauthorized use of this software without a valid license
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * http://www.shopmodule.com
+ * See LICENSE file for license details.
  *
  * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
- * @author    D3 Data Development - Max Buhe <support@shopmodule.com>
- * @link      http://www.oxidmodule.com
+ * @author        D3 Data Development - Max Buhe <support@shopmodule.com>
+ * @link          http://www.oxidmodule.com
  */
 
 namespace D3\PdfDocuments\Application\Model\Registries;
