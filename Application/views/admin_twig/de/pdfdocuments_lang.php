ce05a811a90115e571a495299e8fea2c32b6c716
MaxBuhe01 <mb@shopmodule.com>
2024-09-02 22:57:16 +0200

    [Added] twig multilang

---
+++ Application/views/admin_twig/de/pdfdocuments_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * See LICENSE file for license details.
+ * 
+ * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
+ * @author        D3 Data Development - Daniel Seifert <support@shopmodule.com>
+ * @link          http://www.oxidmodule.com
+ */
+
+$sLangName = "Deutsch";
+$aLang     = array(
+    'charset'                                            => 'utf-8',
+
+    'SHOP_MODULE_GROUP_d3PdfDocumentsmain'               => 'Grundeinstellungen',
+    'SHOP_MODULE_d3PdfDocumentsbDev'                     => 'Entwicklermodus',
+
+    'D3_PDFDOCUMENTS'                                    => 'PDF-Dokumente',
+    'D3_PDFDOCUMENTS_INVOICE'                            => 'Rechnung',
+    'D3_PDFDOCUMENTS_INVOICE_WITHOUT_LOGO'               => 'Rechnung ohne Logo',
+    'D3_PDFDOCUMENTS_DELIVERYNOTE'                       => 'Lieferschein',
+    'D3_PDFDOCUMENTS_DELIVERYNOTE_WITHOUT_LOGO'          => 'Lieferschein ohne Logo',
+
+    'D3_PDFDOCUMENTS_PDF_TYPE'                           => 'Dokument',
+    'D3_PDFDOCUMENTS_LANGUAGE'                           => 'Sprache',
+    'D3_PDFDOCUMENTS_PDF_GENERATE'                       => 'Dokument erstellen',
+);
---
+++ Application/views/admin_twig/en/pdfdocuments_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * See LICENSE file for license details.
+ * 
+ * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
+ * @author        D3 Data Development - Daniel Seifert <support@shopmodule.com>
+ * @link          http://www.oxidmodule.com
+ */
+
+$sLangName = "English";
+$aLang     = array(
+    'charset'                                            => 'utf-8',
+
+    'SHOP_MODULE_GROUP_d3PdfDocumentsmain'               => 'Basic settings',
+    'SHOP_MODULE_d3PdfDocumentsbDev'                     => 'Developer mode',
+
+    'D3_PDFDOCUMENTS'                                    => 'PDF Documents',
+    'D3_PDFDOCUMENTS_INVOICE'                            => 'Invoice',
+    'D3_PDFDOCUMENTS_INVOICE_WITHOUT_LOGO'               => 'Invoice without logo',
+    'D3_PDFDOCUMENTS_DELIVERYNOTE'                       => 'Deliverynote',
+    'D3_PDFDOCUMENTS_DELIVERYNOTE_WITHOUT_LOGO'          => 'Deliverynote without logo',
+
+    'D3_PDFDOCUMENTS_PDF_TYPE'                           => 'Document',
+    'D3_PDFDOCUMENTS_LANGUAGE'                           => 'Language',
+    'D3_PDFDOCUMENTS_PDF_GENERATE'                       => 'Create Document',
+);
