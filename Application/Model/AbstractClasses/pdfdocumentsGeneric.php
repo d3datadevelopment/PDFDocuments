0ee24f08d2da53c896bb6b6b2b70fa9cbbe9dcb1
Daniel Seifert <git@daniel-seifert.com>
2025-05-14 13:22:10 +0200

    change case of 3rd party library methods
    # Conflicts:
    #	Application/Model/AbstractClasses/pdfdocumentsGeneric.php
    
    # Conflicts:
    #	Application/Model/AbstractClasses/pdfdocumentsGeneric.php

--- Application/Model/AbstractClasses/pdfdocumentsGeneric.php
+++ Application/Model/AbstractClasses/pdfdocumentsGeneric.php
@@ -21,6 +21,7 @@ use Smarty;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\MyPdf;
+use Symfony\Component\String\UnicodeString;
 
 abstract class pdfdocumentsGeneric extends Base implements genericInterface
 {
