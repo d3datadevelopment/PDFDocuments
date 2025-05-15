2ab279afbd1e331caa1179e60050057b57052301
Daniel Seifert <git@daniel-seifert.com>
2025-05-15 21:11:14 +0200

    add PHPUnuhi

--- Application/translations/en/pdfdocuments_lang.php
+++ Application/translations/en/pdfdocuments_lang.php
@@ -11,70 +11,10 @@
  * @link      https://www.oxidmodule.com
  */
 
+// @codeCoverageIgnoreStart
+
 $sLangName = "English";
-$aLang     = [
-    'charset'                                            => 'utf-8',
 
-    'D3_PDFDOCUMENTS_THANKYOU_1'                         => 'Many thanks for your purchase!',
-    'D3_PDFDOCUMENTS_THANKYOU_2'                         => 'Your team from %1$s',
+$aLang = include __DIR__."/pdfdocuments_translations.php";
 
-    'D3_PDFDOCUMENTS_GET_IN_CONTACT'                     => 'You can contact us as follows',
-    'D3_PDFDOCUMENTS_TELEFON'                            => 'Telephone',
-    'D3_PDFDOCUMENTS_FAX'                                => 'Fax',
-    'D3_PDFDOCUMENTS_EMAIL'                              => 'Email',
-
-    'D3_PDFDOCUMENTS_BANK_ACCOUNT'                       => 'Bank Account',
-    'D3_PDFDOCUMENTS_BANK_ACCOUNTNR'                     => 'IBAN',
-    'D3_PDFDOCUMENTS_BANK_BANKCODE_HEADER'               => 'BIC',
-
-    'D3_PDFDOCUMENTS_ORDERBILLNR'                        => 'Invoice No.',
-    'D3_PDFDOCUMENTS_CUSTOMERNR'                         => 'Customer No.',
-    'D3_PDFDOCUMENTS_DATE'                               => 'Date',
-    'D3_PDFDOCUMENTS_DATE_FORMAT'                        => '%Y-%m-%d',
-    'D3_PDFDOCUMENTS_USTIDNR'                            => 'VAT ID No.',
-
-    'D3_PDFDOCUMENTS_DELIVERYADDRESS'                    => 'Shipping Address',
-
-    'D3_PDFDOCUMENTS_INVOICE'                            => 'Invoice',
-    'D3_PDFDOCUMENTS_INVOICE_WITHOUT_LOGO'               => 'Invoice without logo',
-    'D3_PDFDOCUMENTS_DELIVERYNOTE'                       => 'Deliverynote',
-    'D3_PDFDOCUMENTS_DELIVERYNOTE_WITHOUT_LOGO'          => 'Deliverynote without logo',
-    'D3_PDFDOCUMENTS_ORDERNR'                            => 'Order No.',
-    'D3_PDFDOCUMENTS_ORDER_FROM_AT'                      => 'Your order from %1$s at "%2$s"',
-
-    'D3_PDFDOCUMENTS_AMOUNT'                             => 'Amount',
-    'D3_PDFDOCUMENTS_DESCRIPTION'                        => 'Description',
-    'D3_PDFDOCUMENTS_USTPERCENTAGE'                      => 'VAT %',
-    'D3_PDFDOCUMENTS_UNITPRICE'                          => 'Unit Price',
-    'D3_PDFDOCUMENTS_TOTALPRICE'                         => 'Total',
-
-    'D3_PDFDOCUMENTS_ARTNR'                              => 'Item No.',
-
-    'D3_PDFDOCUMENTS_SUMBRUTTO'                          => 'Total products (incl. tax)',
-    'D3_PDFDOCUMENTS_DISCOUNT'                           => 'Discount',
-    'D3_PDFDOCUMENTS_SUMNETTO'                           => 'Total products (excl. tax)',
-    'D3_PDFDOCUMENTS_TAX'                                => 'plus VAT %1$s %%',
-    'D3_PDFDOCUMENTS_UNDEFINED_TAX'                      => 'plus VAT',
-    'D3_PDFDOCUMENTS_PROPORTIONAL_TAX'                   => 'plus VAT (proportionally calculated)',
-    'D3_PDFDOCUMENTS_VOUCHERDISCOUNT'                    => 'Coupon',
-    'D3_PDFDOCUMENTS_DELIVERY_NET'                       => 'Shipping costs (excl. tax)',
-    'D3_PDFDOCUMENTS_DELIVERY'                           => 'Shipping costs',
-    'D3_PDFDOCUMENTS_PAYMENT_NET'                        => 'Additional Charge Payment Method (excl. tax)',
-    'D3_PDFDOCUMENTS_PAYMENT'                            => 'Additional Charge Payment Method',
-    'D3_PDFDOCUMENTS_WRAPPING_NET'                       => 'Gift Wrapping (excl. tax)',
-    'D3_PDFDOCUMENTS_WRAPPING'                           => 'Gift Wrapping',
-    'D3_PDFDOCUMENTS_GIFTCARD_NET'                       => 'Greeting Card (excl. tax)',
-    'D3_PDFDOCUMENTS_GIFTCARD'                           => 'Greeting Card',
-    'D3_PDFDOCUMENTS_TOTALSUMBRUT'                       => 'Total sum (gross)',
-
-    'D3_PDFDOCUMENTS_USED_PAYMENTMETHOD'                 => 'Your chosen payment method',
-    'D3_PDFDOCUMENTS_PAYABLEUNTIL'                       => 'Billing till',
-    'D3_PDFDOCUMENTS_RECEIVED_PAYMENT'                   => 'We have received the invoice amount with thanks.',
-
-    'D3_PDFDOCUMENTS_MANAGINGDIRECTOR'                   => 'Managing director',
-    'D3_PDFDOCUMENTS_COURT'                              => 'Local court',
-    'D3_PDFDOCUMENTS_HRBNR'                              => 'HRB',
-    'D3_PDFDOCUMENTS_USTID'                              => 'VAT ID',
-
-    'D3_PDFDOCUMENTS_BANK_BANKCODE'                      => 'BIC/SWIFT Code',
-];
+// @codeCoverageIgnoreEnd
