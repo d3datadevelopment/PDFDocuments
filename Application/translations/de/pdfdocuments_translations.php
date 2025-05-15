dd225b96b965b58754c2ce0726260b28600cfb55
Daniel Seifert <git@daniel-seifert.com>
2025-05-15 21:11:14 +0200

    add PHPUnuhi

---
+++ Application/translations/de/pdfdocuments_translations.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * https://www.d3data.de
+ *
+ * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
+ * @author    D3 Data Development - Daniel Seifert <info@shopmodule.com>
+ * @link      https://www.oxidmodule.com
+ */
+
+$sLangName = "Deutsch";
+return [
+    'charset'                                            => 'utf-8',
+
+    'D3_PDFDOCUMENTS_THANKYOU_1'                         => 'Vielen Dank für Ihren Einkauf!',
+    'D3_PDFDOCUMENTS_THANKYOU_2'                         => 'Ihr Team von %1$s',
+
+    'D3_PDFDOCUMENTS_GET_IN_CONTACT'                     => 'So erreichen Sie uns:',
+    'D3_PDFDOCUMENTS_TELEFON'                            => 'Telefon',
+    'D3_PDFDOCUMENTS_FAX'                                => 'Fax',
+    'D3_PDFDOCUMENTS_EMAIL'                              => 'e-Mail',
+
+    'D3_PDFDOCUMENTS_BANK_ACCOUNT'                       => 'Bankverbindung',
+    'D3_PDFDOCUMENTS_BANK_ACCOUNTNR'                     => 'IBAN',
+    'D3_PDFDOCUMENTS_BANK_BANKCODE_HEADER'               => 'BIC',
+
+    'D3_PDFDOCUMENTS_ORDERBILLNR'                        => 'Rechnungs-Nr.',
+    'D3_PDFDOCUMENTS_CUSTOMERNR'                         => 'Kundennummer',
+    'D3_PDFDOCUMENTS_DATE'                               => 'Datum',
+    'D3_PDFDOCUMENTS_DATE_FORMAT'                        => '%d.%m.%Y',
+    'D3_PDFDOCUMENTS_USTIDNR'                            => 'Ust.-ID-Nr.',
+
+    'D3_PDFDOCUMENTS_DELIVERYADDRESS'                    => 'Lieferadresse',
+
+    'D3_PDFDOCUMENTS_INVOICE'                            => 'Rechnung',
+    'D3_PDFDOCUMENTS_INVOICE_WITHOUT_LOGO'               => 'Rechnung ohne Logo',
+    'D3_PDFDOCUMENTS_DELIVERYNOTE'                       => 'Lieferschein',
+    'D3_PDFDOCUMENTS_DELIVERYNOTE_WITHOUT_LOGO'          => 'Lieferschein ohne Logo',
+    'D3_PDFDOCUMENTS_ORDERNR'                            => 'Bestellnr.',
+    'D3_PDFDOCUMENTS_ORDER_FROM_AT'                      => 'Ihre Bestellung vom %1$s bei "%2$s"',
+
+    'D3_PDFDOCUMENTS_AMOUNT'                             => 'Menge',
+    'D3_PDFDOCUMENTS_DESCRIPTION'                        => 'Beschreibung',
+    'D3_PDFDOCUMENTS_USTPERCENTAGE'                      => 'Ust. %',
+    'D3_PDFDOCUMENTS_UNITPRICE'                          => 'Einzelpreis',
+    'D3_PDFDOCUMENTS_TOTALPRICE'                         => 'Gesamtpreis',
+
+    'D3_PDFDOCUMENTS_ARTNR'                              => 'Art.-Nr.',
+
+    'D3_PDFDOCUMENTS_SUMBRUTTO'                          => 'Summe Artikel (brutto)',
+    'D3_PDFDOCUMENTS_DISCOUNT'                           => 'Rabatt',
+    'D3_PDFDOCUMENTS_SUMNETTO'                           => 'Summe Artikel (netto)',
+    'D3_PDFDOCUMENTS_TAX'                                => 'zzgl. MwSt. %1$s %%',
+    'D3_PDFDOCUMENTS_UNDEFINED_TAX'                      => 'zzgl. MwSt.',
+    'D3_PDFDOCUMENTS_PROPORTIONAL_TAX'                   => 'zzgl. MwSt. (anteilig berechnet)',
+    'D3_PDFDOCUMENTS_VOUCHERDISCOUNT'                    => 'Gutschein',
+    'D3_PDFDOCUMENTS_DELIVERY_NET'                       => 'Versandkosten (netto)',
+    'D3_PDFDOCUMENTS_DELIVERY'                           => 'Versandkosten',
+    'D3_PDFDOCUMENTS_PAYMENT_NET'                        => 'Aufschlag Zahlungsart (netto)',
+    'D3_PDFDOCUMENTS_PAYMENT'                            => 'Aufschlag Zahlungsart',
+    'D3_PDFDOCUMENTS_WRAPPING_NET'                       => 'Geschenkverpackung (netto)',
+    'D3_PDFDOCUMENTS_WRAPPING'                           => 'Geschenkverpackung',
+    'D3_PDFDOCUMENTS_GIFTCARD_NET'                       => 'Grußkarte (netto)',
+    'D3_PDFDOCUMENTS_GIFTCARD'                           => 'Grußkarte',
+    'D3_PDFDOCUMENTS_TOTALSUMBRUT'                       => 'Gesamtsumme (brutto)',
+
+    'D3_PDFDOCUMENTS_USED_PAYMENTMETHOD'                 => 'Ihre gewählte Zahlungsweise',
+    'D3_PDFDOCUMENTS_PAYABLEUNTIL'                       => 'Zahlbar bis zum',
+    'D3_PDFDOCUMENTS_RECEIVED_PAYMENT'                   => 'Den Rechnungsbetrag haben wir dankend erhalten.',
+
+    'D3_PDFDOCUMENTS_MANAGINGDIRECTOR'                   => 'Geschäftsführer',
+    'D3_PDFDOCUMENTS_COURT'                              => 'Amtsgericht',
+    'D3_PDFDOCUMENTS_HRBNR'                              => 'HRB',
+    'D3_PDFDOCUMENTS_USTID'                              => 'Ust.-ID',
+
+    'D3_PDFDOCUMENTS_BANK_BANKCODE'                      => 'BIC/SWIFT-Code',
+];
