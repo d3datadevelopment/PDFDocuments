3a3e8acc0fa04b1d7c73fa989e9b12d30bc31ed1
Daniel Seifert <git@daniel-seifert.com>
2025-05-17 22:12:02 +0200

    select included documents for generating
    # Conflicts:
    #	Application/views/admin/de/pdfdocuments_lang.php
    #	Application/views/admin/en/pdfdocuments_lang.php
    #	metadata.php

--- Application/Model/Registries/registryOrderoverview.php
+++ Application/Model/Registries/registryOrderoverview.php
@@ -15,13 +15,16 @@ declare(strict_types=1);
 
 namespace D3\PdfDocuments\Application\Model\Registries;
 
+use D3\PdfDocuments\Application\Model\Constants;
 use D3\PdfDocuments\Application\Model\Documents\deliverynotePdf;
 use D3\PdfDocuments\Application\Model\Documents\deliverynotewithoutlogoPdf;
 use D3\PdfDocuments\Application\Model\Documents\invoicePdf;
 use D3\PdfDocuments\Application\Model\Documents\invoicewithoutlogoPdf;
 use D3\PdfDocuments\Application\Model\Exceptions\wrongPdfGeneratorInterface;
 use D3\PdfDocuments\Application\Model\Interfaces\pdfdocumentsOrderInterface;
-use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Facade\ModuleSettingService;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Facade\ModuleSettingServiceInterface;
 
 class registryOrderoverview extends registryAbstract implements registryOrderoverviewInterface
 {
@@ -30,14 +33,15 @@ class registryOrderoverview extends registryAbstract implements registryOrderove
      */
     public function __construct()
     {
-        $config = Registry::getConfig();
-        if ($config->getConfigParam('d3PdfDocumentsDocInvoice'))
-            $this->addGenerator(invoicePdf::class);
-        if ($config->getConfigParam('d3PdfDocumentsDocDeliveryNote'))
+        /** @var ModuleSettingService $settingsService */
+        $settingsService =  ContainerFactory::getInstance()->getContainer()->get(ModuleSettingServiceInterface::class);
+        if ($settingsService->getBoolean('d3PdfDocumentsDocInvoice', Constants::OXID_MODULE_ID))
+            $this->addGenerator( invoicePdf::class );
+        if ($settingsService->getBoolean('d3PdfDocumentsDocDeliveryNote', Constants::OXID_MODULE_ID))
             $this->addGenerator(deliverynotePdf::class);
-        if ($config->getConfigParam('d3PdfDocumentsDocInvoiceNoLogo'))
+        if ($settingsService->getBoolean('d3PdfDocumentsDocInvoiceNoLogo', Constants::OXID_MODULE_ID))
             $this->addGenerator(invoicewithoutlogoPdf::class);
-        if ($config->getConfigParam('d3PdfDocumentsDocDeliveryNoteNoLogo'))
+        if ($settingsService->getBoolean('d3PdfDocumentsDocDeliveryNoteNoLogo', Constants::OXID_MODULE_ID))
             $this->addGenerator(deliverynotewithoutlogoPdf::class);
     }
 
